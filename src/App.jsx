4dfa20fea3e5720fe7a8a4a702b3d694cad8ef92
alexreyes091 <alexreyes091@gmail.com>
2022-06-09 19:29:18 -0600

    Configuracion Basica y Prueba

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,21 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="contenedor">
+      <h2>Control de Gastos</h2>
+      <p>Hola! Bienvenido.</p>
+
+      <div className="main">
+        <div className="main__card">
+          <div className="main__title-card">
+            <span>Estado actual:</span>
+          </div>
+          <div className="main__body-card">
+            <span>L. 10,000.00</span>
+          </div>
+        </div>
+        {/* .main__card */}
+      </div>
+      {/* .main */}
     </div>
   )
 }
